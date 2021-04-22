c26690b71ad91a4af90f8fa8b1531bc0d08acb42
Jason Smigovsky <jsmigovsky@fearless.tech>
2021-04-22 10:41:15 -0400

    IA-1918: added a daily backup vault, items will be removed after 30 days

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+# Local Vars
+locals {
+  enabled_count = var.enabled ? 1 : 0
+
+  daily_backup_enabled = var.enabled && var.daily_backup_enabled
+  daily_backup_count   = var.enabled && var.daily_backup_enabled ? 1 : 0
+
+  weekly_backup_enabled = var.enabled && var.weekly_backup_enabled
+  weekly_backup_count   = var.enabled && var.weekly_backup_enabled ? 1 : 0
+
+  quarterly_backup_enabled = var.enabled && var.quarterly_backup_enabled
+  quarterly_backup_count   = var.enabled && var.quarterly_backup_enabled ? 1 : 0
+}
+
+# Assume Role Poliy for AWS Backup Service
 data "aws_iam_policy_document" "service_link" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -7,42 +22,82 @@ data "aws_iam_policy_document" "service_link" {
     }
   }
 }
-
-locals {
-  enabled_count = var.enabled ? 1 : 0
-}
 resource "aws_iam_role" "service_role" {
   count              = local.enabled_count
   name               = "backup-service-role"
   assume_role_policy = data.aws_iam_policy_document.service_link.json
 }
-
 resource "aws_iam_role_policy_attachment" "service_role_attachment" {
   count      = local.enabled_count
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup"
   role       = var.enabled ? aws_iam_role.service_role[0].name : ""
 }
 
-
-## Weekly Plan
-
-locals {
-  weekly_backup_enabled = var.enabled && var.weekly_backup_enabled
-  weekly_backup_count   = var.enabled && var.weekly_backup_enabled ? 1 : 0
+## Daily Backup Plan
+# - Runs every day @ 08:00 AM UTC
+# - Backups are removed after 30 days
+resource "aws_backup_vault" "daily" {
+  count = local.daily_backup_count
+  name  = "daily"
+  tags  = merge(var.tags, var.tags_vault)
 }
+resource "aws_backup_vault" "daily_cross_region" {
+  count    = var.cross_region_backup_enabled ? local.daily_backup_count : 0
+  name     = "daily_cross_region"
+  tags     = merge(var.tags, var.tags_vault)
+  provider = aws.cross-region
+}
+resource "aws_backup_plan" "daily" {
+  count = local.daily_backup_count
+  name  = "daily"
+  tags  = merge(var.tags, var.tags_plan)
+
+  rule {
+    rule_name         = "daily"
+    target_vault_name = aws_backup_vault.daily[0].name
+    schedule          = "cron(0 8 ? * * *)"
+
+    lifecycle {
+      delete_after = 30
+    }
+    dynamic "copy_action" {
+      for_each = var.cross_region_backup_enabled ? ["copy backups to the new region"] : []
+      content {
+        destination_vault_arn = aws_backup_vault.daily_cross_region[0].arn
+        lifecycle {
+          delete_after = 30
+        }
+      }
+    }
+  }
+}
+resource "aws_backup_selection" "daily" {
+  count        = local.daily_backup_count
+  iam_role_arn = aws_iam_role.service_role[0].arn
+  name         = "daily"
+  plan_id      = aws_backup_plan.daily[0].id
+
+  selection_tag {
+    type  = "STRINGEQUALS"
+    key   = var.daily_backup_tag_key
+    value = var.daily_backup_tag_value
+  }
+}
+
+## Weekly Backup Plan
+# - Runs every week on SUNDAY @ 12:00 AM UTC
+# - Backups are removed after 90 days
 resource "aws_backup_vault" "weekly" {
   count = local.weekly_backup_count
   name  = "weekly"
   tags  = merge(var.tags, var.tags_vault)
 }
-
 resource "aws_backup_vault" "weekly_cross_region" {
   count    = var.cross_region_backup_enabled ? local.weekly_backup_count : 0
   name     = "weekly_cross_region"
   tags     = merge(var.tags, var.tags_vault)
   provider = aws.cross-region
 }
-
 resource "aws_backup_plan" "weekly" {
   count = local.weekly_backup_count
   name  = "weekly"
@@ -67,7 +122,6 @@ resource "aws_backup_plan" "weekly" {
     }
   }
 }
-
 resource "aws_backup_selection" "weekly" {
   count        = local.weekly_backup_count
   iam_role_arn = aws_iam_role.service_role[0].arn
@@ -81,25 +135,20 @@ resource "aws_backup_selection" "weekly" {
   }
 }
 
-# Quarterly Plan
-
-locals {
-  quarterly_backup_enabled = var.enabled && var.quarterly_backup_enabled
-  quarterly_backup_count   = var.enabled && var.quarterly_backup_enabled ? 1 : 0
-}
+## Quarterly Plan
+# - Run every quarter on SUN 12:00:00 AM UTC (SAT 07:00:00 PM EST)
+# - Run every quarter on the LAST day of MARCH, JUNE, SEPTEMBER, DECEMBER @ 12:00 AM UTC
 resource "aws_backup_vault" "quarterly" {
   count = local.quarterly_backup_count
   name  = "quarterly"
   tags  = merge(var.tags, var.tags_vault)
 }
-
 resource "aws_backup_vault" "quarterly_cross_region" {
   count    = var.cross_region_backup_enabled ? local.quarterly_backup_count : 0
   name     = "quarterly_cross_region"
   tags     = merge(var.tags, var.tags_vault)
   provider = aws.cross-region
 }
-
 resource "aws_backup_plan" "quarterly" {
   count = local.quarterly_backup_count
   name  = "quarterly"
@@ -124,7 +173,6 @@ resource "aws_backup_plan" "quarterly" {
     }
   }
 }
-
 resource "aws_backup_selection" "quarterly" {
   count        = local.quarterly_backup_count
   iam_role_arn = aws_iam_role.service_role[0].arn
@@ -138,6 +186,7 @@ resource "aws_backup_selection" "quarterly" {
   }
 }
 
+# Opt-In Settings
 resource "aws_backup_region_settings" "opt_in" {
   resource_type_opt_in_preference = {
     "DynamoDB"        = true
--- variables.tf
+++ variables.tf
@@ -3,36 +3,41 @@ variable "enabled" {
   description = "enable/disable creation of all resources in this module"
   default     = true
 }
-variable "weekly_backup_enabled" {
-  type        = bool
-  description = "enable/disable weekly backups"
-  default     = true
-}
-variable "quarterly_backup_enabled" {
-  type        = bool
-  description = "enable/disable quarterly backups"
-  default     = true
-}
 
-variable "cross_region_destination" {
-  type        = string
-  description = "The region of the cross-region backup copy.  Default is 'us-west-2'"
-  default     = "us-west-2"
-}
+# cross region settings
 variable "cross_region_backup_enabled" {
   type        = bool
   description = "enable/disable cross-region backups.  Defaults to 'false'"
   default     = false
 }
-variable "quarterly_backup_tag_key" {
+variable "cross_region_destination" {
   type        = string
-  description = "Tag Key for backing up resources quarterly"
-  default     = "BackupQuarterly"
+  description = "The region of the cross-region backup copy.  Default is 'us-west-2'"
+  default     = "us-west-2"
 }
-variable "quarterly_backup_tag_value" {
+
+# daily backup settings
+variable "daily_backup_enabled" {
+  type        = bool
+  description = "enable/disable daily backups"
+  default     = true
+}
+variable "daily_backup_tag_key" {
   type        = string
-  description = "Tag Value for backing up resources quarterly"
+  description = "Tag Key for backing up resources daily"
+  default     = "BackupDaily"
+}
+variable "daily_backup_tag_value" {
+  type        = string
+  description = "Tag Value for backing up resources daily"
   default     = "true"
+}
+
+# weekly backup settings
+variable "weekly_backup_enabled" {
+  type        = bool
+  description = "enable/disable weekly backups"
+  default     = true
 }
 variable "weekly_backup_tag_key" {
   type        = string
@@ -45,6 +50,24 @@ variable "weekly_backup_tag_value" {
   default     = "true"
 }
 
+# quarterly backup settings
+variable "quarterly_backup_enabled" {
+  type        = bool
+  description = "enable/disable quarterly backups"
+  default     = true
+}
+variable "quarterly_backup_tag_key" {
+  type        = string
+  description = "Tag Key for backing up resources quarterly"
+  default     = "BackupQuarterly"
+}
+variable "quarterly_backup_tag_value" {
+  type        = string
+  description = "Tag Value for backing up resources quarterly"
+  default     = "true"
+}
+
+# tagging
 variable "tags" {
   type        = map(any)
   description = "Optional; Map of key-value tags to apply to all applicable resources"
