b34e02d1bf6de9e522d85ed6f9934ff573d90ddd
Jason Smigovsky <jsmigovsky@lithesolutions.com>
2025-07-29 09:24:29 -0400

    IA-4592: removed var.enabled checks from locals.tf

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
   # SNS notification count - only create if SNS topic is provided
-  sns_notification_count = var.enabled && var.daily_backup_enabled && var.sns_topic_arn != "" ? 1 : 0
+  sns_notification_count = var.sns_topic_arn != "" ? 1 : 0
 }
